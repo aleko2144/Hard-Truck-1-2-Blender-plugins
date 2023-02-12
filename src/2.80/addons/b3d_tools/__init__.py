f5085f308a2afe5e162933d0121bf09071a418dc
LabVaKars <ivancar1234567890@gmail.com>
2023-02-12 11:57:50 +0200

    v.2.3.1

--- src/2.80/addons/b3d_tools/__init__.py
+++ src/2.80/addons/b3d_tools/__init__.py
@@ -53,7 +53,7 @@ bl_info = {
 	"name": "King of The Road Tools",
 	"description": "",
 	"author": "Andrey Prozhoga, LabVaKars",
-	"version": (1, 3, 1),
+	"version": (2, 3, 1),
 	"blender": (2, 93, 0),
 	"location": "3D View > Tools",
 	"warning": "",
